1648e7c5a7c530c667af27443055a0d7a9eed0a6
Mikko Johannes Koivunalho <mikko.koivunalho@inoviagroup.se>
2019-05-03 09:05:25 +0200

    Add tools/get-check.sh

---
+++ tools/get-check.sh
@@ -0,0 +1,15 @@
+set -x
+git clone https://github.com/libcheck/check.git
+cd check
+git remote add mikkoi git@github.com:mikkoi/check.git
+git fetch mikkoi
+git checkout improve-import
+cd ..
+mkdir -p build-Release-check
+rm -Rf build-Release-check/*
+mkdir -p install-Release-check
+rm -Rf install-Release-check/*
+cd build-Release-check
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install-Release-check ../check
+make install
+
