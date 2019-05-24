bb7b1ecf59aaa41be0599547f952c2a80a0b218d
Mikko Johannes Koivunalho <mikko.koivunalho@inoviagroup.se>
2019-05-24 16:19:08 +0200

    Fix test_suite.h

--- src/test_suite.h
+++ src/test_suite.h
@@ -1,8 +1,10 @@
 #ifndef RUBIK_TEST_SUITE
 #define RUBIK_TEST_SUITE
 
+#include <check.h>
+
 #define MAX_TESTS_IN_SUITE 20
-void add_tests(size_t const n, void (* tests[n])(int));
+void add_tests(size_t const n, TFun tests[n]);
 
 #endif
 
