107b16aca7f43fd9918a65259080836aa18b28d8
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2019-06-06 20:18:13 +0200

    Deactivate Cppcheck for all

--- cppcheck.cmake
+++ cppcheck.cmake
@@ -2,7 +2,7 @@
 
 # get all project files
 # HACK this workaround is required to avoid qml files checking ^_^
-file(GLOB_RECURSE ALL_SOURCE_FILES misc/*.c ModernC/*.c AOX-C/*.c)
+file(GLOB_RECURSE ALL_SOURCE_FILES src/*.c)
 # foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
 #     string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
 #     if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
@@ -12,20 +12,21 @@ file(GLOB_RECURSE ALL_SOURCE_FILES misc/*.c ModernC/*.c AOX-C/*.c)
 
 add_custom_target(
         cppcheck
-        ALL
+        # ALL
         COMMENT "Running Cppcheck on all files."
         COMMAND cppcheck
-            --enable=all
-            #--enable=warning,performance,portability,information,missingInclude
-            --language=c
-            --std=c89
+            # --enable=all
+            # --enable=warning,performance,portability,information,missingInclude
+            --enable=warning,performance,portability,information
+            --language=c --std=c11
             #--library=qt.cfg
             --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
             # --platform=unix64
-            --verbose
-            --quiet
-            --inline-suppr
+            --verbose --quiet --inline-suppr
+            # --check-config
+            # -I ${CMAKE_C_STANDARD_INCLUDE_DIRECTORIES}
             ${ALL_SOURCE_FILES}
             # -i ${CMAKE_CURRENT_SOURCE_DIR}/otaDATA
 )
+# message(STATUS "include_dirs:${CMAKE_C_STANDARD_INCLUDE_DIRECTORIES}")
 # add_custom_command    COMMAND cppcheck --enable all --
