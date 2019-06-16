73ec3bbfe5d6a3b681cf08c78f675e38e7da8f3f
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2019-06-16 08:44:53 +0200

    Use getopt and rubik.h in bin/rubik

--- src/bin/rubik.c
+++ src/bin/rubik.c
@@ -1,11 +1,46 @@
+/* Activate getopt */
+#define _POSIX_C_SOURCE 2
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
 #include <locale.h>
+
+#include <unistd.h>
+
 #include "../project.h"
+#include <rubik.h>
 #include "../tui/core.h"
 
-int main(int argc, RUBIK_ATTRIBUTE_UNUSED char* argv[argc+1]) {
+void print_usage(char const*);
+void print_usage(char const* program_name) {
+    fprintf(stderr, "Usage: %s [-hlw] [file...]\n", program_name);
+}
+
+int main(int argc, char* argv[argc+1]) {
     setlocale(LC_ALL, "");
+    int opt;
+
+    while ((opt = getopt(argc, argv, "Vh")) != -1) {
+        switch (opt) {
+        case 'V':
+            fprintf(stdout, "%s\n", RUBIK_PROJECT_VERSION);
+            exit(EXIT_SUCCESS);
+            break;
+        case 'h':
+            print_usage(argv[0]);
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+
+    // Now optind (declared extern int by <unistd.h>) is the index of the first non-option argument.
+    // If it is >= argc, there were no non-option arguments.
+
     if(ncurses_run() > 0)
         return EXIT_SUCCESS;
     else
